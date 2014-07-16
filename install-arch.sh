borh/nexus-7-2013-arch-scripts

commit 0d3919ba4e4cbb20dfb90376059100f90356f652

download BusyBox binary from official website to avoid problems

install-arch.sh
@@ -2,45 +2,51 @@
 
 CHROOT="/data/arch"
 
+echo "Downloading official BusyBox from busybox.net..."
+mkdir -p "/data/local/tmp/arch/"
+busybox wget http://busybox.net/downloads/binaries/latest/busybox-armv7l -O "/data/local/tmp/arch/arch-busybox"
+chmod 777 "/data/local/tmp/arch/arch-busybox"
+PATH="$PATH:/data/local/tmp/arch/"
+
 echo "Downloading ArchLinuxARM-trimslice-latest.tar.gz..."
-busybox wget -c http://tw.mirror.archlinuxarm.org/os/ArchLinuxARM-trimslice-latest.tar.gz
+arch-busybox wget -c http://tw.mirror.archlinuxarm.org/os/ArchLinuxARM-trimslice-latest.tar.gz
 
 echo "Making 10GB image..."
-busybox dd if=/dev/zero of=${CHROOT}.img bs=1M seek=10000 count=1
+arch-busybox dd if=/dev/zero of=${CHROOT}.img bs=1M seek=10000 count=1
 
 echo "Formating image..."
-busybox mke2fs -L arch-image -F ${CHROOT}.img
-busybox mkdir ${CHROOT}
+arch-busybox mke2fs -L arch-image -F ${CHROOT}.img
+arch-busybox mkdir ${CHROOT}
 
 echo "Making devices..."
-busybox mknod /dev/loop256 b 7 256
-busybox losetup /dev/loop256 ${CHROOT}.img || exit 1
+arch-busybox mknod /dev/loop256 b 7 256
+arch-busybox losetup /dev/loop256 ${CHROOT}.img || exit 1
 
 echo "Mounting file systems..."
-busybox mount -t ext4 -o rw,noatime /dev/block/loop256 ${CHROOT} || exit 1
+arch-busybox mount -t ext4 -o rw,noatime /dev/block/loop256 ${CHROOT} || exit 1
 
 echo "Extracting Arch system into image..."
-busybox gunzip ArchLinuxARM-trimslice-latest.tar.gz -c | busybox tar x -f - -C ${CHROOT}
+arch-busybox gunzip ArchLinuxARM-trimslice-latest.tar.gz -c | arch-busybox tar x -f - -C ${CHROOT}
 
 echo "Adding directories..."
-busybox mkdir -p ${CHROOT}/media/sdcard
-busybox mkdir -p ${CHROOT}/media/system
-busybox mkdir -p ${CHROOT}/dev/pts
-busybox mkdir ${CHROOT}/dev/ptmx
+arch-busybox mkdir -p ${CHROOT}/media/sdcard
+arch-busybox mkdir -p ${CHROOT}/media/system
+arch-busybox mkdir -p ${CHROOT}/dev/pts
+arch-busybox mkdir ${CHROOT}/dev/ptmx
 
 echo "Mounting extra file systems..."
-busybox mount -o bind /dev/ ${CHROOT}/dev || exit 1
-busybox mount -t proc proc ${CHROOT}/proc || exit 1
-busybox mount -t sysfs sysfs ${CHROOT}/sys || exit 1
-busybox mount -t devpts devpts ${CHROOT}/dev/pts || exit 1
-busybox mount -o bind /sdcard ${CHROOT}/media/sdcard || exit 1
+arch-busybox mount -o bind /dev/ ${CHROOT}/dev || exit 1
+arch-busybox mount -t proc proc ${CHROOT}/proc || exit 1
+arch-busybox mount -t sysfs sysfs ${CHROOT}/sys || exit 1
+arch-busybox mount -t devpts devpts ${CHROOT}/dev/pts || exit 1
+arch-busybox mount -o bind /sdcard ${CHROOT}/media/sdcard || exit 1
 
 echo "Installing packages and upgrading system..."
 echo "nameserver 8.8.8.8" > ${CHROOT}/etc/resolv.conf
 echo "nexus7" > ${CHROOT}/etc/hostname
-busybox rm -rf ${CHROOT}/opt/nvidia
-busybox rm -f ${CHROOT}/etc/ld.so.conf.d/nvidia-trimslice.conf
-busybox chroot ${CHROOT} /usr/bin/env HOME=/root TERM="$TERM" PATH=/bin:/usr/bin:/sbin:/usr/sbin /bin/bash -c "source /etc/profile; groupadd -g 3003 aid_inet; groupadd -g 3004 inet; groupadd -g 3005 inetadmin; usermod -aG inet root; usermod -aG inetadmin root; pacman -Syyu --noconfirm; pacman -S --noconfirm xorg-server xorg-xrdb tigervnc xterm xorg-xsetroot xorg-xmodmap rxvt-unicode dwm pkg-config dmenu fakeroot zsh emacs vim git tmux mosh ruby python3 python2 sudo wget rsync base-devel; pacman -Rdd --noconfirm linux-firmware"
+arch-busybox rm -rf ${CHROOT}/opt/nvidia
+arch-busybox rm -f ${CHROOT}/etc/ld.so.conf.d/nvidia-trimslice.conf
+arch-busybox chroot ${CHROOT} /usr/bin/env HOME=/root TERM="$TERM" PATH=/bin:/usr/bin:/sbin:/usr/sbin /bin/bash -c "source /etc/profile; groupadd -g 3003 aid_inet; groupadd -g 3004 inet; groupadd -g 3005 inetadmin; usermod -aG inet root; usermod -aG inetadmin root; pacman -Syyu --noconfirm; pacman -S --noconfirm xorg-server xorg-xrdb tigervnc xterm xorg-xsetroot xorg-xmodmap rxvt-unicode dwm pkg-config dmenu fakeroot zsh emacs vim git tmux mosh ruby python3 python2 sudo wget rsync base-devel; pacman -Rdd --noconfirm linux-firmware"
 
 echo "Exiting chroot, unmounting..."
 umount ${CHROOT}/media/sdcard || exit 1
@@ -49,12 +55,12 @@ umount ${CHROOT}/proc || exit 1
 umount ${CHROOT}/sys || exit 1
 
 echo "Killing existing processes in chroot..."
-busybox fuser -mk ${CHROOT}
+arch-busybox fuser -mk ${CHROOT}
 umount ${CHROOT}/dev || exit 1
 umount ${CHROOT} || exit 1
 
 echo "Deactivating loop..."
-busybox losetup -d /dev/loop256
+arch-busybox losetup -d /dev/loop256
 
 #echo "Deleting ArchLinuxARM-trimslice-latest.tar.gz..."
 #rm ArchLinuxARM-trimslice-latest.tar.gz

commit c3a55dabf654177dc5b6f6fd4e0c58e231863a16

fix a bug introduced with 0d3919b

install-arch.sh
@@ -5,48 +5,48 @@ CHROOT="/data/arch"
 echo "Downloading official BusyBox from busybox.net..."
 mkdir -p "/data/local/tmp/arch/"
 busybox wget http://busybox.net/downloads/binaries/latest/busybox-armv7l -O "/data/local/tmp/arch/arch-busybox" || exit 1
-chmod 777 "/data/local/tmp/arch/arch-busybox"
-PATH="$PATH:/data/local/tmp/arch/"
+chmod 777 "/data/local/tmp/arch/archbusybox"
+busybox="/data/local/tmp/arch/archbusybox"
 
 echo "Downloading ArchLinuxARM-trimslice-latest.tar.gz..."
-arch-busybox wget -c http://tw.mirror.archlinuxarm.org/os/ArchLinuxARM-trimslice-latest.tar.gz || exit 1
+$archbusybox wget -c http://nl.mirror.archlinuxarm.org/os/ArchLinuxARM-trimslice-latest.tar.gz || exit 1
 
-echo "Making 10GB image..."
-arch-busybox dd if=/dev/zero of=${CHROOT}.img bs=1M seek=10000 count=1 || exit 1
+echo "Making 4GB image..."
+$archbusybox dd if=/dev/zero of=${CHROOT}.img bs=1M seek=4096 count=1 || exit 1
 
-echo "Formating image..."
-arch-busybox mke2fs -L arch-image -F ${CHROOT}.img || exit 1
-arch-busybox mkdir ${CHROOT}
+echo "Formatting image..."
+$archbusybox mke2fs -L arch-image -F ${CHROOT}.img || exit 1
+$archbusybox mkdir ${CHROOT}
 
 echo "Making devices..."
-arch-busybox mknod /dev/loop256 b 7 256 || exit 1
-arch-busybox losetup /dev/loop256 ${CHROOT}.img || exit 1
+$archbusybox mknod /dev/loop256 b 7 256 || exit 1
+$archbusybox losetup /dev/loop256 ${CHROOT}.img || exit 1
 
 echo "Mounting file systems..."
-arch-busybox mount -t ext4 -o rw,noatime /dev/block/loop256 ${CHROOT} || exit 1
+$archbusybox mount -t ext4 -o rw,noatime /dev/block/loop256 ${CHROOT} || exit 1
 
 echo "Extracting Arch system into image..."
-arch-busybox gunzip ArchLinuxARM-trimslice-latest.tar.gz -c | arch-busybox tar x -f - -C ${CHROOT} || exit 1
+$archbusybox gunzip ArchLinuxARM-trimslice-latest.tar.gz -c | $archbusybox tar x -f - -C ${CHROOT} || exit 1
 
 echo "Adding directories..."
-arch-busybox mkdir -p ${CHROOT}/media/sdcard
-arch-busybox mkdir -p ${CHROOT}/media/system
-arch-busybox mkdir -p ${CHROOT}/dev/pts
-arch-busybox mkdir ${CHROOT}/dev/ptmx
+$archbusybox mkdir -p ${CHROOT}/media/sdcard
+$archbusybox mkdir -p ${CHROOT}/media/system
+$archbusybox mkdir -p ${CHROOT}/dev/pts
+$archbusybox mkdir ${CHROOT}/dev/ptmx
 
 echo "Mounting extra file systems..."
-arch-busybox mount -o bind /dev/ ${CHROOT}/dev || exit 1
-arch-busybox mount -t proc proc ${CHROOT}/proc || exit 1
-arch-busybox mount -t sysfs sysfs ${CHROOT}/sys || exit 1
-arch-busybox mount -t devpts devpts ${CHROOT}/dev/pts || exit 1
-arch-busybox mount -o bind /sdcard ${CHROOT}/media/sdcard || exit 1
+$archbusybox mount -o bind /dev/ ${CHROOT}/dev || exit 1
+$archbusybox mount -t proc proc ${CHROOT}/proc || exit 1
+$archbusybox mount -t sysfs sysfs ${CHROOT}/sys || exit 1
+$archbusybox mount -t devpts devpts ${CHROOT}/dev/pts || exit 1
+$archbusybox mount -o bind /sdcard ${CHROOT}/media/sdcard || exit 1
 
 echo "Installing packages and upgrading system..."
-echo "nameserver 8.8.8.8" > ${CHROOT}/etc/resolv.conf
+echo -e "nameserver 208.67.222.222\nnameserver 208.67.220.220" > ${CHROOT}/etc/resolv.conf
 echo "nexus7" > ${CHROOT}/etc/hostname
-arch-busybox rm -rf ${CHROOT}/opt/nvidia
-arch-busybox rm -f ${CHROOT}/etc/ld.so.conf.d/nvidia-trimslice.conf
-arch-busybox chroot ${CHROOT} /usr/bin/env HOME=/root TERM="$TERM" PATH=/bin:/usr/bin:/sbin:/usr/sbin /bin/bash -c "source /etc/profile; groupadd -g 3003 aid_inet; groupadd -g 3004 inet; groupadd -g 3005 inetadmin; usermod -aG inet root; usermod -aG inetadmin root; pacman -Syyu --noconfirm; pacman -S --noconfirm xorg-server xorg-xrdb tigervnc xterm xorg-xsetroot xorg-xmodmap rxvt-unicode dwm pkg-config dmenu fakeroot zsh emacs vim git tmux mosh ruby python3 python2 sudo wget rsync base-devel; pacman -Rdd --noconfirm linux-firmware"
+$archbusybox rm -rf ${CHROOT}/opt/nvidia
+$archbusybox rm -f ${CHROOT}/etc/ld.so.conf.d/nvidia-trimslice.conf
+$archbusybox chroot ${CHROOT} /usr/bin/env HOME=/root TERM="$TERM" PATH=/bin:/usr/bin:/sbin:/usr/sbin /bin/bash -c "source /etc/profile; groupadd -g 3003 aid_inet; groupadd -g 3004 inet; groupadd -g 3005 inetadmin; usermod -aG inet root; usermod -aG inetadmin root; pacman -Syyu --noconfirm; pacman -S --noconfirm xorg-server xorg-xrdb tigervnc xterm xorg-xsetroot xorg-xmodmap rxvt-unicode dwm pkg-config dmenu fakeroot zsh emacs vim git tmux mosh ruby python3 python2 sudo wget rsync base-devel; pacman -Rdd --noconfirm linux-firmware"
 
 echo "Exiting chroot, unmounting..."
 umount ${CHROOT}/media/sdcard
@@ -55,12 +55,12 @@ umount ${CHROOT}/proc
 umount ${CHROOT}/sys
 
 echo "Killing existing processes in chroot..."
-arch-busybox fuser -mk ${CHROOT}
+$archbusybox fuser -mk ${CHROOT}
 umount ${CHROOT}/dev
 umount ${CHROOT}
 
 echo "Deactivating loop..."
-arch-busybox losetup -d /dev/loop256
+$archbusybox losetup -d /dev/loop256
 
 #echo "Deleting ArchLinuxARM-trimslice-latest.tar.gz..."
 #rm ArchLinuxARM-trimslice-latest.tar.gz